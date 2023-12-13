ab740d1fa0d8c4f81906d31f0736dedb4e05e816
Michael Rozenblat <michael.rozenblat@pendo.io>
2023-12-13 14:23:27 -0500

    first commit

--- src/Calculator/App.xaml.cs
+++ src/Calculator/App.xaml.cs
@@ -18,7 +18,7 @@ public partial class App : Application
         verify the Pendo instance is not null */
         if (Pendo != null)
         {
-            string apiKey = "eyJhbGciOiJSUzI1NiIsImtpZCI6IiIsInR5cCI6IkpXVCJ9.eyJkYXRhY2VudGVyIjoidXMiLCJrZXkiOiI1YTg5ODk2NTQ3ZWM5ODFiY2E0ZWFkYjFiZmVlYjMzZTlmM2YyMDdiZWU3Mjk4NGUxOThjYjVlODhlYWFkY2VjOWQyNmZkZDIwNGFkOGQ3ZGIyMTVhYjFjZDJiODJjZTI4YmIzYmFjYzExNzRkODNjYzE4NThmMWQxNTBhNTk2OC45YjI1YTIxNTQ4OTY4ZmUyY2E5YTc1ZGQ4ZWJmNGQxNS4xYjFlYmQ5NDU3MjEyODkzMzg4YjgzNzVkZmQyY2EyOGQzN2NmNTdkODFjNjNkNjFmMWI4NjYzNDE2ZGUwMDc0In0.Iua2npvVckTNxft1jJGBis4N_qNzlU0GPn5Wlh0E9mFxJN1ZpfxAy23bWkwtA07xWvq2CpEZ6lDWzLdy7c2je8oXYDSJMwn7Kl4FUdNkJIvpN2PfwGKht9BR5rfGUnqderN52VWXh8zWgh38uYxyel3kozmmfm_XsN0sYOwiTJ4";
+            string apiKey = "key";
             Pendo.SetDebugMode(true);
             Pendo.Setup(apiKey);
         }
